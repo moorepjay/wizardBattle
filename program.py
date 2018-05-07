1149496efac4c557dd783840f5d060d980f2dc3b
Preston Moore <moorepjay@gmail.com>
2018-05-06 22:08:08 -0400

    Added basic classes.

--- program.py
+++ program.py
@@ -1,3 +1,4 @@
+from actors import Wizard, Creature
 
 
 def main():
@@ -12,6 +13,16 @@ def print_header():
 
 
 def game_loop():
+
+    creatures = [
+        Creature("Toad", 1),
+        Creature("Tiger", 12),
+        Creature("Bat", 3),
+        Creature("Dragon", 50),
+        Creature("Evil Wizard", 1000),
+    ]
+
+    hero = Wizard("Alouris", 75)
 
     while True:
         cmd = input("Do you [a]ttack, [r]un away, or [l]ook around? ")
